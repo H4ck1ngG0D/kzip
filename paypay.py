001a4e3b01c824865840146d2ba4308c624fa658
H4ck1ngG0D <kamichitataro1254@gmail.com>
2025-07-29 14:53:57 +0900

    Update paypay.py

--- paypay.py
+++ paypay.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import json
 import os
-from paypay import PayPay  # PayPaython-mobile をインポート
+from PayPaython_mobile import PayPay
 
 intents = discord.Intents.default()
 intents.message_content = True
